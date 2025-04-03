31bd655e447891c99cf9567b06d8223c7690d85b
july yukari <julyukari.cdd@gmail.com>
2025-04-03 11:53:55 -0300

    desafio01

---
+++ atividade01.py
@@ -0,0 +1,18 @@
+hora1= int(input("digite a hora: "))
+minuto1= int(input("digite o minuto: "))
+hora2= int(input(f"digite a hora: "))
+minuto2= int(input(f"digite o minuto: "))
+somaminuto= minuto1 + minuto2
+somahora= hora1 + hora2
+ if somaminuto >=60
+     somahora +1
+
+
+
+
+
+
+
+
+
+

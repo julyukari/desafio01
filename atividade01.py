b73f52dc9a3a1623c582a22f9538a7de8659f30a
julyukari <julyukari.cdd@gmail.com>
2025-04-06 19:52:32 -0300

    DESAFIO

--- atividade01.py
+++ atividade01.py
@@ -3,9 +3,19 @@ minuto1= int(input("digite o minuto: "))
 hora2= int(input(f"digite a hora: "))
 minuto2= int(input(f"digite o minuto: "))
 somaminuto= minuto1 + minuto2
+minuto= somaminuto
 somahora= hora1 + hora2
- if somaminuto >=60
-     somahora +1
+if hora= somahora >=60
+if minuto <=60
+
+
+
+
+
+
+
+
+
 
 
 

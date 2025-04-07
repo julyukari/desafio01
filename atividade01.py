02794457a3c99eb87cec4bdb910f19b5759a2119
julyukari <julyukari.cdd@gmail.com>
2025-04-07 20:30:47 -0300

    atividade01

--- atividade01.py
+++ atividade01.py
@@ -1,13 +1,22 @@
+# Dados os valores de horario a baixo, decifre a logica e faca um codigo para executar
+# entrada 1 3:45 - entrada 2: 14:20 - saida:6:05
 hora1= int(input("digite a hora: "))
 minuto1= int(input("digite o minuto: "))
-hora2= int(input(f"digite a hora: "))
-minuto2= int(input(f"digite o minuto: "))
-somaminuto= minuto1 + minuto2
-minuto= somaminuto
+hora2= int(input("digite a hora: "))
+minuto2= int(input("digite o minuto: "))
 somahora= hora1 + hora2
-if hora= somahora >=60
-if minuto <=60
-
+somaminuto= minuto1 + minuto2
+if somaminuto >=60:
+   somahora += 1
+if somaminuto >=60:
+   somaminuto -= 60
+if hora1 >= 12:
+   somahora -= 12
+if hora2 >= 12:
+   somahora -= 12
+if somahora >= 12:
+   somahora -= 12
+print(f"são extamente {somahora}:{somaminuto} min ")
 
 
 
